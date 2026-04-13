8b781f1b341ea2bcc59e03befa3bcab5e8954cc5
ibnrosheed <ibnrosheed9@gmail.com>
2026-04-13 08:07:38 +0100

    added login form

--- script/script.js
+++ script/script.js
@@ -16,5 +16,34 @@ window.onclick = function (event) {
     }
 };
 
+// Sign in button functionality
+const signinBtn = document.getElementById('signinBtn');
+signinBtn.addEventListener('click', function () {
+    // alert('Sign in functionality is not implemented yet.');
+    const loginForm = document.getElementById('loginForm');
+    if (loginForm.style.display === 'block') {
+        loginForm.style.display = 'none';
+    }
+    else {
+        loginForm.style.display = 'block';
+    }
+}
+);
+
+//  Close the login form if the user clicks outside of it
+window.onclick = function (event) {
+    const loginForm = document.getElementById('loginForm');
+    if (!event.target.matches('#signinBtn') && !event.target.matches('#signinBtn *') && !event.target.closest('#loginForm')) {
+        loginForm.style.display = 'none';
+    }
+};
+
+//  Close button functionality for the login form
+const closeLoginForm = document.getElementById('closeLoginForm');
+closeLoginForm.addEventListener('click', function () {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.style.display = 'none';
+});
+
 
 
